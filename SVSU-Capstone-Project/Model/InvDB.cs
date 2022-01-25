99416f9173cd37ac59a81e650952e34cc90f6e8c
Devin Campbell <dcampbell@wadetrim.com>
2022-01-25 10:31:51 -0500

    init model folder dbcontext class

---
+++ SVSU-Capstone-Project/Model/InvDB.cs
@@ -0,0 +1,14 @@
+
+
+
+public class InvDbContext
+{
+    // Create a connection to the database
+
+    // Define allowable methods for interface to the database
+
+    // Manage Crud Methods for database objects
+}
+
+
+// Define Schema as seperate class files under the Model folder
