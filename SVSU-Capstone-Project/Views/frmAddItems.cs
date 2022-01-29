59ac6d84b1e993f5be940f064b26abccda3ca8cb
badluckbert <bcprevid@svsu.edu>
2022-01-29 17:32:40 -0500

    Add controls to frmAddItems and Created ManageLocations
    frmManageLocations is used for when an item will have multiple locations. A button will appear when that option is selected. Intention is to take you to that child form, and take you back to AddItems when you save/cancel location changes.
    
    To do:
    Set Manage Locations button to take you to frmMultipleLocaitons.
    Set Save/Cancel to return you to frmAddItems.
    frmMultipleLocations will display the current item's name in the title label.

--- SVSU-Capstone-Project/Views/frmAddItems.cs
+++ SVSU-Capstone-Project/Views/frmAddItems.cs
@@ -16,5 +16,69 @@ namespace SVSU_Capstone_Project.Views
         {
             InitializeComponent();
         }
+
+        private void frmAddItems_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnManageLocations_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PageController(object sender, EventArgs e)
+        {
+            //Instantiate two forms (old and new)
+            Form oldF = ActiveMdiChild, newF = null;
+
+            //If a child (old) form already exists
+            if (oldF != null)
+            {
+                //Don't do anything if the child is already open
+                if (newF.Name == oldF.Name)
+                {
+                    return;
+                }
+
+                //Close the current form
+                oldF.Close();
+            }
+
+            //Show new child form
+            newF.MdiParent = this;
+            newF.FormBorderStyle = FormBorderStyle.None;
+            newF.Show();
+            newF.Location = new Point(0, 0);
+            newF.Size = new Size(971, 492);
+        }
+
+        private void chkMultipleLocations_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMultipleLocations.Checked)
+            {
+                cmbLocation.Visible = false;
+                cmbCabinet.Enabled = false;
+                btnManageLocations.Visible = true;
+            }
+            else
+            {
+                cmbLocation.Visible = true;
+                cmbCabinet.Enabled = true;
+                btnManageLocations.Visible = false;
+            }
+        }
+
+        private void chkNLevel_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkNLevel.Checked)
+            {
+                cmbNLevel.Enabled = true;
+            }
+            else
+            {
+                cmbNLevel.Enabled = false;
+            }
+        }
     }
 }
