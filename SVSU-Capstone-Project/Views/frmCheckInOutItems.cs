d9cbccc4c6b2658f907941ccb6347ad5bf86d435
MaxGervias <70655933+MaxGervias@users.noreply.github.com>
2022-01-29 16:06:47 -0500

    Changes to Item checkout page

--- SVSU-Capstone-Project/Views/frmCheckInOutItems.cs
+++ SVSU-Capstone-Project/Views/frmCheckInOutItems.cs
@@ -16,5 +16,66 @@ namespace SVSU_Capstone_Project.Views
         {
             InitializeComponent();
         }
+
+       
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox14_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
