d81bc77cd74020c214a8e41355a7b01994f220e1
badluckbert <bcprevid@svsu.edu>
2022-01-29 15:29:27 -0500

    Adding AddItem Functionality

--- SVSU-Capstone-Project/Views/frmAddItems.Designer.cs
+++ SVSU-Capstone-Project/Views/frmAddItems.Designer.cs
@@ -29,24 +29,299 @@ namespace SVSU_Capstone_Project.Views
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.RichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.TextBox14 = new System.Windows.Forms.TextBox();
+            this.Label10 = new System.Windows.Forms.Label();
+            this.ComboBox5 = new System.Windows.Forms.ComboBox();
+            this.Label15 = new System.Windows.Forms.Label();
+            this.TextBox10 = new System.Windows.Forms.TextBox();
+            this.Label24 = new System.Windows.Forms.Label();
+            this.TextBox11 = new System.Windows.Forms.TextBox();
+            this.Label27 = new System.Windows.Forms.Label();
+            this.Label28 = new System.Windows.Forms.Label();
+            this.ComboBox4 = new System.Windows.Forms.ComboBox();
+            this.Label29 = new System.Windows.Forms.Label();
+            this.CheckBox1 = new System.Windows.Forms.CheckBox();
+            this.ComboBox3 = new System.Windows.Forms.ComboBox();
+            this.Label30 = new System.Windows.Forms.Label();
+            this.ComboBox2 = new System.Windows.Forms.ComboBox();
+            this.Label31 = new System.Windows.Forms.Label();
+            this.Label32 = new System.Windows.Forms.Label();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.Label33 = new System.Windows.Forms.Label();
+            this.TextBox12 = new System.Windows.Forms.TextBox();
+            this.Label34 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // TextBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "add items page";
+            this.TextBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox1.Location = new System.Drawing.Point(110, 73);
+            this.TextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(55, 26);
+            this.TextBox1.TabIndex = 83;
+            this.TextBox1.Text = "0";
+            // 
+            // RichTextBox1
+            // 
+            this.RichTextBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RichTextBox1.Location = new System.Drawing.Point(110, 344);
+            this.RichTextBox1.Name = "RichTextBox1";
+            this.RichTextBox1.Size = new System.Drawing.Size(192, 91);
+            this.RichTextBox1.TabIndex = 82;
+            this.RichTextBox1.Text = "";
+            // 
+            // TextBox14
+            // 
+            this.TextBox14.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox14.Location = new System.Drawing.Point(110, 206);
+            this.TextBox14.Margin = new System.Windows.Forms.Padding(2);
+            this.TextBox14.Name = "TextBox14";
+            this.TextBox14.Size = new System.Drawing.Size(55, 26);
+            this.TextBox14.TabIndex = 81;
+            this.TextBox14.Text = "0";
+            // 
+            // Label10
+            // 
+            this.Label10.AutoSize = true;
+            this.Label10.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label10.Location = new System.Drawing.Point(9, 342);
+            this.Label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label10.Name = "Label10";
+            this.Label10.Size = new System.Drawing.Size(103, 19);
+            this.Label10.TabIndex = 80;
+            this.Label10.Text = "Description:";
+            // 
+            // ComboBox5
+            // 
+            this.ComboBox5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBox5.FormattingEnabled = true;
+            this.ComboBox5.Location = new System.Drawing.Point(110, 306);
+            this.ComboBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.ComboBox5.Name = "ComboBox5";
+            this.ComboBox5.Size = new System.Drawing.Size(111, 26);
+            this.ComboBox5.TabIndex = 79;
+            // 
+            // Label15
+            // 
+            this.Label15.AutoSize = true;
+            this.Label15.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label15.Location = new System.Drawing.Point(22, 307);
+            this.Label15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label15.Name = "Label15";
+            this.Label15.Size = new System.Drawing.Size(82, 19);
+            this.Label15.TabIndex = 78;
+            this.Label15.Text = "Features:";
+            // 
+            // TextBox10
+            // 
+            this.TextBox10.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox10.Location = new System.Drawing.Point(110, 275);
+            this.TextBox10.Margin = new System.Windows.Forms.Padding(2);
+            this.TextBox10.Name = "TextBox10";
+            this.TextBox10.Size = new System.Drawing.Size(55, 26);
+            this.TextBox10.TabIndex = 77;
+            this.TextBox10.Text = "0";
+            // 
+            // Label24
+            // 
+            this.Label24.AutoSize = true;
+            this.Label24.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label24.Location = new System.Drawing.Point(15, 276);
+            this.Label24.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label24.Name = "Label24";
+            this.Label24.Size = new System.Drawing.Size(96, 19);
+            this.Label24.TabIndex = 76;
+            this.Label24.Text = "Low Stock:";
+            // 
+            // TextBox11
+            // 
+            this.TextBox11.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox11.Location = new System.Drawing.Point(110, 240);
+            this.TextBox11.Margin = new System.Windows.Forms.Padding(2);
+            this.TextBox11.Name = "TextBox11";
+            this.TextBox11.Size = new System.Drawing.Size(112, 26);
+            this.TextBox11.TabIndex = 75;
+            // 
+            // Label27
+            // 
+            this.Label27.AutoSize = true;
+            this.Label27.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label27.Location = new System.Drawing.Point(50, 241);
+            this.Label27.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label27.Name = "Label27";
+            this.Label27.Size = new System.Drawing.Size(48, 19);
+            this.Label27.TabIndex = 74;
+            this.Label27.Text = "Link:";
+            // 
+            // Label28
+            // 
+            this.Label28.AutoSize = true;
+            this.Label28.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label28.Location = new System.Drawing.Point(50, 207);
+            this.Label28.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label28.Name = "Label28";
+            this.Label28.Size = new System.Drawing.Size(51, 19);
+            this.Label28.TabIndex = 73;
+            this.Label28.Text = "Cost:";
+            // 
+            // ComboBox4
+            // 
+            this.ComboBox4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBox4.FormattingEnabled = true;
+            this.ComboBox4.Location = new System.Drawing.Point(110, 171);
+            this.ComboBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.ComboBox4.Name = "ComboBox4";
+            this.ComboBox4.Size = new System.Drawing.Size(113, 26);
+            this.ComboBox4.TabIndex = 72;
+            // 
+            // Label29
+            // 
+            this.Label29.AutoSize = true;
+            this.Label29.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label29.Location = new System.Drawing.Point(27, 172);
+            this.Label29.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label29.Name = "Label29";
+            this.Label29.Size = new System.Drawing.Size(79, 19);
+            this.Label29.TabIndex = 71;
+            this.Label29.Text = "Supplier:";
+            // 
+            // CheckBox1
+            // 
+            this.CheckBox1.AutoSize = true;
+            this.CheckBox1.Checked = true;
+            this.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckBox1.Location = new System.Drawing.Point(110, 141);
+            this.CheckBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.CheckBox1.Name = "CheckBox1";
+            this.CheckBox1.Size = new System.Drawing.Size(15, 14);
+            this.CheckBox1.TabIndex = 70;
+            this.CheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // ComboBox3
+            // 
+            this.ComboBox3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBox3.FormattingEnabled = true;
+            this.ComboBox3.Location = new System.Drawing.Point(129, 136);
+            this.ComboBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.ComboBox3.Name = "ComboBox3";
+            this.ComboBox3.Size = new System.Drawing.Size(38, 26);
+            this.ComboBox3.TabIndex = 69;
+            // 
+            // Label30
+            // 
+            this.Label30.AutoSize = true;
+            this.Label30.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label30.Location = new System.Drawing.Point(31, 139);
+            this.Label30.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label30.Name = "Label30";
+            this.Label30.Size = new System.Drawing.Size(72, 19);
+            this.Label30.TabIndex = 68;
+            this.Label30.Text = "N Level:";
+            // 
+            // ComboBox2
+            // 
+            this.ComboBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBox2.FormattingEnabled = true;
+            this.ComboBox2.Location = new System.Drawing.Point(110, 104);
+            this.ComboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.ComboBox2.Name = "ComboBox2";
+            this.ComboBox2.Size = new System.Drawing.Size(113, 26);
+            this.ComboBox2.TabIndex = 67;
+            // 
+            // Label31
+            // 
+            this.Label31.AutoSize = true;
+            this.Label31.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label31.Location = new System.Drawing.Point(25, 106);
+            this.Label31.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label31.Name = "Label31";
+            this.Label31.Size = new System.Drawing.Size(82, 19);
+            this.Label31.TabIndex = 66;
+            this.Label31.Text = "Location:";
+            // 
+            // Label32
+            // 
+            this.Label32.AutoSize = true;
+            this.Label32.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label32.Location = new System.Drawing.Point(25, 74);
+            this.Label32.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label32.Name = "Label32";
+            this.Label32.Size = new System.Drawing.Size(79, 19);
+            this.Label32.TabIndex = 65;
+            this.Label32.Text = "Quantity:";
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Location = new System.Drawing.Point(110, 41);
+            this.ComboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(115, 26);
+            this.ComboBox1.TabIndex = 64;
+            // 
+            // Label33
+            // 
+            this.Label33.AutoSize = true;
+            this.Label33.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label33.Location = new System.Drawing.Point(21, 42);
+            this.Label33.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label33.Name = "Label33";
+            this.Label33.Size = new System.Drawing.Size(85, 19);
+            this.Label33.TabIndex = 63;
+            this.Label33.Text = "Category:";
+            // 
+            // TextBox12
+            // 
+            this.TextBox12.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox12.Location = new System.Drawing.Point(110, 8);
+            this.TextBox12.Margin = new System.Windows.Forms.Padding(2);
+            this.TextBox12.Name = "TextBox12";
+            this.TextBox12.Size = new System.Drawing.Size(117, 26);
+            this.TextBox12.TabIndex = 62;
+            // 
+            // Label34
+            // 
+            this.Label34.AutoSize = true;
+            this.Label34.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label34.Location = new System.Drawing.Point(11, 9);
+            this.Label34.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label34.Name = "Label34";
+            this.Label34.Size = new System.Drawing.Size(95, 19);
+            this.Label34.TabIndex = 61;
+            this.Label34.Text = "Item Name:";
             // 
             // frmAddItems
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(955, 453);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.RichTextBox1);
+            this.Controls.Add(this.TextBox14);
+            this.Controls.Add(this.Label10);
+            this.Controls.Add(this.ComboBox5);
+            this.Controls.Add(this.Label15);
+            this.Controls.Add(this.TextBox10);
+            this.Controls.Add(this.Label24);
+            this.Controls.Add(this.TextBox11);
+            this.Controls.Add(this.Label27);
+            this.Controls.Add(this.Label28);
+            this.Controls.Add(this.ComboBox4);
+            this.Controls.Add(this.Label29);
+            this.Controls.Add(this.CheckBox1);
+            this.Controls.Add(this.ComboBox3);
+            this.Controls.Add(this.Label30);
+            this.Controls.Add(this.ComboBox2);
+            this.Controls.Add(this.Label31);
+            this.Controls.Add(this.Label32);
+            this.Controls.Add(this.ComboBox1);
+            this.Controls.Add(this.Label33);
+            this.Controls.Add(this.TextBox12);
+            this.Controls.Add(this.Label34);
             this.Name = "frmAddItems";
             this.Text = "Add Items";
             this.ResumeLayout(false);
@@ -56,6 +331,28 @@ namespace SVSU_Capstone_Project.Views
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.RichTextBox RichTextBox1;
+        internal System.Windows.Forms.TextBox TextBox14;
+        internal System.Windows.Forms.Label Label10;
+        internal System.Windows.Forms.ComboBox ComboBox5;
+        internal System.Windows.Forms.Label Label15;
+        internal System.Windows.Forms.TextBox TextBox10;
+        internal System.Windows.Forms.Label Label24;
+        internal System.Windows.Forms.TextBox TextBox11;
+        internal System.Windows.Forms.Label Label27;
+        internal System.Windows.Forms.Label Label28;
+        internal System.Windows.Forms.ComboBox ComboBox4;
+        internal System.Windows.Forms.Label Label29;
+        internal System.Windows.Forms.CheckBox CheckBox1;
+        internal System.Windows.Forms.ComboBox ComboBox3;
+        internal System.Windows.Forms.Label Label30;
+        internal System.Windows.Forms.ComboBox ComboBox2;
+        internal System.Windows.Forms.Label Label31;
+        internal System.Windows.Forms.Label Label32;
+        internal System.Windows.Forms.ComboBox ComboBox1;
+        internal System.Windows.Forms.Label Label33;
+        internal System.Windows.Forms.TextBox TextBox12;
+        internal System.Windows.Forms.Label Label34;
     }
 }
