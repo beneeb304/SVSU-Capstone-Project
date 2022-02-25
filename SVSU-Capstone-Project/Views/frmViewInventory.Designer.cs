a5ba2f4d5539d51c1d063c73ba245fade00d6010
Benjamin Neeb <beneeb304@gmail.com>
2022-02-24 21:32:51 -0500

    Added filtering for categories

--- SVSU-Capstone-Project/Views/frmViewInventory.Designer.cs
+++ SVSU-Capstone-Project/Views/frmViewInventory.Designer.cs
@@ -30,7 +30,7 @@ namespace SVSU_Capstone_Project.Views
         private void InitializeComponent()
         {
             this.lblViewInventory = new System.Windows.Forms.Label();
-            this.btnExportToExcel = new System.Windows.Forms.Button();
+            this.btnPrintBarcode = new System.Windows.Forms.Button();
             this.grbFilterFields = new System.Windows.Forms.GroupBox();
             this.cmbVendor = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -43,12 +43,6 @@ namespace SVSU_Capstone_Project.Views
             this.lblSearch = new System.Windows.Forms.Label();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.dgvInventoryView = new System.Windows.Forms.DataGridView();
-            //this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            //this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            //this.Location = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            //this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            //this.Supplier = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            //this.NLevel = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.grbFilterFields.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventoryView)).BeginInit();
             this.SuspendLayout();
@@ -59,21 +53,22 @@ namespace SVSU_Capstone_Project.Views
             this.lblViewInventory.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblViewInventory.Location = new System.Drawing.Point(12, 13);
             this.lblViewInventory.Name = "lblViewInventory";
-            this.lblViewInventory.Size = new System.Drawing.Size(183, 29);
+            this.lblViewInventory.Size = new System.Drawing.Size(147, 22);
             this.lblViewInventory.TabIndex = 43;
             this.lblViewInventory.Text = "View Inventory";
             // 
-            // btnExportToExcel
+            // btnPrintBarcode
             // 
-            this.btnExportToExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.btnExportToExcel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExportToExcel.Location = new System.Drawing.Point(844, 89);
-            this.btnExportToExcel.Margin = new System.Windows.Forms.Padding(4);
-            this.btnExportToExcel.Name = "btnExportToExcel";
-            this.btnExportToExcel.Size = new System.Drawing.Size(98, 55);
-            this.btnExportToExcel.TabIndex = 6;
-            this.btnExportToExcel.Text = "Export to Excel";
-            this.btnExportToExcel.UseVisualStyleBackColor = true;
+            this.btnPrintBarcode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnPrintBarcode.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrintBarcode.Location = new System.Drawing.Point(844, 89);
+            this.btnPrintBarcode.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrintBarcode.Name = "btnPrintBarcode";
+            this.btnPrintBarcode.Size = new System.Drawing.Size(98, 55);
+            this.btnPrintBarcode.TabIndex = 6;
+            this.btnPrintBarcode.Text = "Print Barcode";
+            this.btnPrintBarcode.UseVisualStyleBackColor = true;
+            this.btnPrintBarcode.Click += new System.EventHandler(this.btnPrintBarcode_Click);
             // 
             // grbFilterFields
             // 
@@ -99,13 +94,16 @@ namespace SVSU_Capstone_Project.Views
             // cmbVendor
             // 
             this.cmbVendor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.cmbVendor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbVendor.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbVendor.FormattingEnabled = true;
             this.cmbVendor.Location = new System.Drawing.Point(474, 48);
             this.cmbVendor.Margin = new System.Windows.Forms.Padding(4);
             this.cmbVendor.Name = "cmbVendor";
-            this.cmbVendor.Size = new System.Drawing.Size(189, 31);
+            this.cmbVendor.Size = new System.Drawing.Size(189, 26);
             this.cmbVendor.TabIndex = 4;
+            this.cmbVendor.SelectedIndexChanged += new System.EventHandler(this.cmbVendor_SelectedIndexChanged);
+            this.cmbVendor.Click += new System.EventHandler(this.ClickCmb);
             // 
             // label3
             // 
@@ -115,20 +113,23 @@ namespace SVSU_Capstone_Project.Views
             this.label3.Location = new System.Drawing.Point(470, 25);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 24);
+            this.label3.Size = new System.Drawing.Size(70, 19);
             this.label3.TabIndex = 46;
             this.label3.Text = "Vendor:";
             // 
             // cmbRoom
             // 
             this.cmbRoom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.cmbRoom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbRoom.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbRoom.FormattingEnabled = true;
             this.cmbRoom.Location = new System.Drawing.Point(285, 48);
             this.cmbRoom.Margin = new System.Windows.Forms.Padding(4);
             this.cmbRoom.Name = "cmbRoom";
-            this.cmbRoom.Size = new System.Drawing.Size(106, 31);
+            this.cmbRoom.Size = new System.Drawing.Size(106, 26);
             this.cmbRoom.TabIndex = 3;
+            this.cmbRoom.SelectedIndexChanged += new System.EventHandler(this.cmbRoom_SelectedIndexChanged);
+            this.cmbRoom.Click += new System.EventHandler(this.ClickCmb);
             // 
             // label2
             // 
@@ -138,21 +139,24 @@ namespace SVSU_Capstone_Project.Views
             this.label2.Location = new System.Drawing.Point(281, 25);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 24);
+            this.label2.Size = new System.Drawing.Size(61, 19);
             this.label2.TabIndex = 44;
             this.label2.Text = "Room:";
             // 
             // cmbCategory
             // 
             this.cmbCategory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbCategory.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbCategory.FormattingEnabled = true;
-            this.cmbCategory.ItemHeight = 23;
+            this.cmbCategory.ItemHeight = 18;
             this.cmbCategory.Location = new System.Drawing.Point(8, 48);
             this.cmbCategory.Margin = new System.Windows.Forms.Padding(4);
             this.cmbCategory.Name = "cmbCategory";
-            this.cmbCategory.Size = new System.Drawing.Size(194, 31);
+            this.cmbCategory.Size = new System.Drawing.Size(194, 26);
             this.cmbCategory.TabIndex = 2;
+            this.cmbCategory.SelectedIndexChanged += new System.EventHandler(this.cmbCategory_SelectedIndexChanged);
+            this.cmbCategory.Click += new System.EventHandler(this.ClickCmb);
             // 
             // label1
             // 
@@ -162,20 +166,23 @@ namespace SVSU_Capstone_Project.Views
             this.label1.Location = new System.Drawing.Point(4, 25);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.Size = new System.Drawing.Size(85, 19);
             this.label1.TabIndex = 42;
             this.label1.Text = "Category:";
             // 
             // cmbNLevel
             // 
             this.cmbNLevel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.cmbNLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbNLevel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbNLevel.FormattingEnabled = true;
             this.cmbNLevel.Location = new System.Drawing.Point(731, 48);
             this.cmbNLevel.Margin = new System.Windows.Forms.Padding(4);
             this.cmbNLevel.Name = "cmbNLevel";
-            this.cmbNLevel.Size = new System.Drawing.Size(84, 31);
+            this.cmbNLevel.Size = new System.Drawing.Size(84, 26);
             this.cmbNLevel.TabIndex = 5;
+            this.cmbNLevel.SelectedIndexChanged += new System.EventHandler(this.cmbNLevel_SelectedIndexChanged);
+            this.cmbNLevel.Click += new System.EventHandler(this.ClickCmb);
             // 
             // lblNLevel
             // 
@@ -185,7 +192,7 @@ namespace SVSU_Capstone_Project.Views
             this.lblNLevel.Location = new System.Drawing.Point(727, 25);
             this.lblNLevel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNLevel.Name = "lblNLevel";
-            this.lblNLevel.Size = new System.Drawing.Size(88, 24);
+            this.lblNLevel.Size = new System.Drawing.Size(73, 19);
             this.lblNLevel.TabIndex = 40;
             this.lblNLevel.Text = "N-Level:";
             // 
@@ -197,7 +204,7 @@ namespace SVSU_Capstone_Project.Views
             this.lblSearch.Location = new System.Drawing.Point(186, 31);
             this.lblSearch.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(76, 24);
+            this.lblSearch.Size = new System.Drawing.Size(63, 19);
             this.lblSearch.TabIndex = 38;
             this.lblSearch.Text = "Search";
             // 
@@ -208,77 +215,43 @@ namespace SVSU_Capstone_Project.Views
             this.txtSearch.Location = new System.Drawing.Point(275, 28);
             this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(325, 30);
+            this.txtSearch.Size = new System.Drawing.Size(325, 26);
             this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
             // dgvInventoryView
             // 
+            this.dgvInventoryView.AllowUserToAddRows = false;
+            this.dgvInventoryView.AllowUserToDeleteRows = false;
+            this.dgvInventoryView.AllowUserToOrderColumns = true;
+            this.dgvInventoryView.AllowUserToResizeColumns = false;
+            this.dgvInventoryView.AllowUserToResizeRows = false;
             this.dgvInventoryView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvInventoryView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            //this.dgvInventoryView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            //this.Name,
-            //this.Category,
-            //this.Location,
-            //this.Quantity,
-            //this.Supplier,
-            //this.NLevel});
             this.dgvInventoryView.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.dgvInventoryView.Location = new System.Drawing.Point(0, 152);
             this.dgvInventoryView.Margin = new System.Windows.Forms.Padding(4);
             this.dgvInventoryView.MultiSelect = false;
             this.dgvInventoryView.Name = "dgvInventoryView";
+            this.dgvInventoryView.ReadOnly = true;
+            this.dgvInventoryView.RowHeadersVisible = false;
             this.dgvInventoryView.RowHeadersWidth = 51;
+            this.dgvInventoryView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvInventoryView.Size = new System.Drawing.Size(955, 301);
             this.dgvInventoryView.TabIndex = 7;
-            /*
-            // 
-            // Name
-            // 
-            this.Name.HeaderText = "Name";
-            this.Name.MinimumWidth = 6;
-            this.Name.Name = "Name";
-            // 
-            // Category
-            // 
-            this.Category.HeaderText = "Category";
-            this.Category.MinimumWidth = 6;
-            this.Category.Name = "Category";
-            // 
-            // Location
-            // 
-            this.Location.HeaderText = "Location";
-            this.Location.MinimumWidth = 6;
-            this.Location.Name = "Location";
-            // 
-            // Quantity
-            // 
-            this.Quantity.HeaderText = "Quantity";
-            this.Quantity.MinimumWidth = 6;
-            this.Quantity.Name = "Quantity";
-            // 
-            // Supplier
-            // 
-            this.Supplier.HeaderText = "Supplier";
-            this.Supplier.MinimumWidth = 6;
-            this.Supplier.Name = "Supplier";
-            // 
-            // NLevel
-            // 
-            this.NLevel.HeaderText = "N Level";
-            this.NLevel.MinimumWidth = 6;
-            this.NLevel.Name = "NLevel";
-            */
+            this.dgvInventoryView.TabStop = false;
             // 
             // frmViewInventory
             // 
             this.ClientSize = new System.Drawing.Size(955, 453);
             this.Controls.Add(this.lblViewInventory);
-            this.Controls.Add(this.btnExportToExcel);
+            this.Controls.Add(this.btnPrintBarcode);
             this.Controls.Add(this.grbFilterFields);
             this.Controls.Add(this.lblSearch);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.dgvInventoryView);
             this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmViewInventory";
             this.Text = "View Inventory";
             this.Load += new System.EventHandler(this.frmViewInventory_Load);
             this.grbFilterFields.ResumeLayout(false);
@@ -292,7 +265,7 @@ namespace SVSU_Capstone_Project.Views
         #endregion
 
         private System.Windows.Forms.Label lblViewInventory;
-        private System.Windows.Forms.Button btnExportToExcel;
+        private System.Windows.Forms.Button btnPrintBarcode;
         private System.Windows.Forms.GroupBox grbFilterFields;
         private System.Windows.Forms.Label lblNLevel;
         private System.Windows.Forms.ComboBox cmbNLevel;
--- SVSU-Capstone-Project/Views/frmViewInventory.cs
+++ SVSU-Capstone-Project/Views/frmViewInventory.cs
@@ -14,6 +14,9 @@ namespace SVSU_Capstone_Project.Views
 {
     public partial class frmViewInventory : Form
     {
+        //List for dgv
+        List<Commodity> lstCommodities;
+        
         public frmViewInventory()
         {
             /* Function: frmLogin
@@ -38,13 +41,88 @@ namespace SVSU_Capstone_Project.Views
         * 
         */
 
-            //populate category dropdown
-            cmbCategory.DataSource = ItemModel.GetMany<Category>(x => x.strName == "Consumable").OrderBy(x => x.strName).ToList();
-            //cmbCommodity.DataSource = (cmbCategory.SelectedValue as Category).lstCommodities;
+            //Set datasource for each combobox
+            cmbCategory.DataSource = ItemModel.GetMany<Category>().OrderBy(x => x.strName).ToList();
+            cmbNLevel.DataSource = ItemModel.GetMany<NLevel>().OrderBy(x => x.strName).ToList();
+            cmbRoom.DataSource = ItemModel.GetMany<Room>().OrderBy(x => x.strName).ToList();
+            cmbVendor.DataSource = ItemModel.GetMany<Vendor>().OrderBy(x => x.strName).ToList();
 
-            //assets
-            dgvInventoryView.DataSource = ItemModel.GetMany<Commodity>(x => x.objCategory.strName == "Asset").OrderBy(x => x.strName).ToList();
+            //Click on the category combobox
+            ClickCmb(cmbCategory, e);
+        }
 
+        private void btnPrintBarcode_Click( object sender, EventArgs e )
+        {
+            if (dgvInventoryView.SelectedRows.Count > 0)
+            {
+                Commodity commodity = ItemModel.Get<Commodity>(x => x.uidTuid.ToString() == dgvInventoryView.Columns[7].HeaderCell.ToString());
+
+                try
+                {
+                    //Print barcode here
+                    MessageBox.Show("For testing purposes, the barcode is " + commodity.strBarCode);
+                }
+                catch
+                {
+                    MessageBox.Show("Barcode is null");
+                }
+            }
+        }
+
+        private void ClickCmb( object sender, EventArgs e )
+        {
+            //Get the clicked combobox
+            var cmbCurrent = sender as ComboBox;
+            
+            //Empty all others
+            foreach (var cmb in Controls.OfType<GroupBox>().SelectMany(groupBox => groupBox.Controls.OfType<ComboBox>()))
+            {
+                if (cmb != cmbCurrent)
+                {
+                    cmb.SelectedIndex = -1;
+                }
+            }
+        }
+
+        private void cmbCategory_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            //Get list
+            lstCommodities = ItemModel.GetMany<Commodity>(x => x.objCategory.strName == cmbCategory.Text).OrderBy(x => x.strName).ToList();
+            
+            //Set dgv
+            dgvInventoryView.DataSource = lstCommodities;
+        }
+
+        private void cmbRoom_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            
+        }
+
+        private void cmbVendor_SelectedIndexChanged( object sender, EventArgs e )
+        {
+
+        }
+
+        private void cmbNLevel_SelectedIndexChanged( object sender, EventArgs e )
+        {
+
+        }
+
+        private void txtSearch_TextChanged( object sender, EventArgs e )
+        {            
+            if(txtSearch.Text.Length > 0)
+            {
+                //Make temp list that is filtered
+                List<Commodity> lstTemp = lstCommodities.Where(x => x.strName.IndexOf(txtSearch.Text, 0, StringComparison.CurrentCultureIgnoreCase) != -1).ToList();
+
+                //Set dgv with filtered datasource
+                dgvInventoryView.DataSource = lstTemp;
+            }
+            else
+            {
+                //Set dgv back to normal
+                dgvInventoryView.DataSource = lstCommodities;
+            }
         }
     }
 }
