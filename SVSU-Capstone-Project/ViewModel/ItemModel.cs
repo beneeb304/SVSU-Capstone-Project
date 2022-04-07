2c8a17d87ee8af31955bddd7a6e6bb5c156be945
Lamar Scott <lamar.scott@nexteer.com>
2022-04-07 11:10:36 -0400

    Stupid merge conflict didnt pull trevors work...

--- SVSU-Capstone-Project/ViewModel/ItemModel.cs
+++ SVSU-Capstone-Project/ViewModel/ItemModel.cs
@@ -39,9 +39,9 @@ namespace SVSU_Capstone_Project.ViewModel
             {
                 MessageBox.Show(ex.Message, "Error! No connection to databse!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 MessageBox.Show("Please check your internet connection and try to start the application again. The application will now close.", "Alert");
-                
+
                 //close application 
-                Environment.Exit(0);              
+                Environment.Exit(0);
             }
             return db.Set<T>().ToList();
         }
@@ -59,17 +59,17 @@ namespace SVSU_Capstone_Project.ViewModel
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error! No connection to databse!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                MessageBox.Show("Please check your internet connection and try again. You can also restart the application and try agian.", "Alert");
-            }
+                MessageBox.Show("Please check your internet connection and try to start the application again. The application will now close.", "Alert");
 
-                return db.Set<T>().FirstOrDefault(predicate);
-            
-            
+                //close application 
+                Environment.Exit(0);
+            }
+            return db.Set<T>().FirstOrDefault(predicate);
         }
 
         public static Guid Add<T>( T obj, out T item ) where T : ContextEntity
         {
-            
+
             //Set tuid
             obj.uidTuid = Guid.NewGuid();
 
@@ -100,7 +100,10 @@ namespace SVSU_Capstone_Project.ViewModel
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error! No connection to databse!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                MessageBox.Show("Please check your internet connection and try again. You can also restart the application and try agian.", "Alert");
+                MessageBox.Show("Please check your internet connection and try to start the application again. The application will now close.", "Alert");
+
+                //close application 
+                Environment.Exit(0);
             }
 
             return Add(obj, out T item);
@@ -120,7 +123,10 @@ namespace SVSU_Capstone_Project.ViewModel
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error! No connection to databse!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                MessageBox.Show("Please check your internet connection and try again. You can also restart the application and try agian.", "Alert");
+                MessageBox.Show("Please check your internet connection and try to start the application again. The application will now close.", "Alert");
+
+                //close application 
+                Environment.Exit(0);
             }
 
             db.Entry(obj).State = System.Data.Entity.EntityState.Modified;
@@ -140,7 +146,10 @@ namespace SVSU_Capstone_Project.ViewModel
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error! No connection to databse!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                MessageBox.Show("Please check your internet connection and try again. You can also restart the application and try agian.", "Alert");
+                MessageBox.Show("Please check your internet connection and try to start the application again. The application will now close.", "Alert");
+
+                //close application 
+                Environment.Exit(0);
             }
 
             db.Set<T>().Remove(obj);
